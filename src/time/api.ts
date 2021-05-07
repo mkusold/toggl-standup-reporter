b321b2607295d85927130b7a929cf2bed345589c
M Kusold <m@kusold.com>
2021-05-07 16:57:07 -0700

    forgot to save this

--- src/time/api.ts
+++ src/time/api.ts
@@ -1,7 +1,6 @@
+import { constructUrl } from "./utils";
+
 export const TOGGL_BASE_URL = 'https://api.track.toggl.com';
 
-export function constructUrl(...urlElements: Array<string>){
-    return urlElements.join('/');
-}
 export const REPORTS_BASE_URL = constructUrl(TOGGL_BASE_URL, 'reports/api/v2');
 export const REPORT_DETAILS_BASE_URL = constructUrl(REPORTS_BASE_URL, 'details');
