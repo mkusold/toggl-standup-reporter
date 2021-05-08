b92b73c899f205d6b34aef544586fa4dd78c2b9a
M Kusold <m@kusold.com>
2021-05-07 17:15:42 -0700

    adding auto-run scheduling

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Are you sick of having to hand-draft a daily remote standup email? This tool int
 1. Run `yarn && cp .env.example .env`
 1. Log into Toggl and go to https://track.toggl.com/profile and copy and paste the API Token into the `.env` file.
 1. We also need the workspace ID which is a bit harder to find. Click on the Settings page in the side nav. Now look in the URL. It should look something like `https://track.toggl.com/{WorkspaceID}/settings/general`. Copy your workspace ID from here into your `.env` file.
+1. To run in the background forever and on startup, run `npm install forever -g`
+1. Then kick off the program by running `forever start -v --minUptime 5000 --spinSleepTime 2000 -c "yarn start" ./`
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 require('dotenv').config()
+import schedule from 'node-schedule';
 import { generateOutput } from './communication/generateOutput';
 import { getYesterdaysWorkSummary } from './time/getTime';
+import { formatDate } from './time/utils';
 
 
-async function main(){
+async function updateReport(){
+    console.log(`${formatDate(new Date())}: Creating Report...`);
     const reportedWork = await getYesterdaysWorkSummary();
-    console.log('work', reportedWork);   
     await generateOutput(reportedWork);
+}
+async function main(){
+    const rule = new schedule.RecurrenceRule();
+    // 8 AM every day
+    rule.hour = 8;
+    rule.minute = 0;
+
+    const job = schedule.scheduleJob(rule, updateReport);
 }
 main();
